fe2cc2bb1b639d8b2669d732de3ab82a93c0ed63
Lucas Pinto <github@lucks.pt>
2023-05-09 18:32:00 +0100

    test: coisas do iptables

--- src/main/java/pt/fcul/sinf/si003/server/iptablesScript.sh
+++ src/main/java/pt/fcul/sinf/si003/server/iptablesScript.sh
@@ -12,11 +12,8 @@ iptables -P OUTPUT DROP
 # Set default chain policies to DROP for forwarding traffic
 iptables -P FORWARD DROP
 
-
-
 # The packets that are allowed are specified below
 # -j flag stands for "jump" and specifies the target of the rule
-
 
 # Allow loopback device traffic 
 #################################
@@ -26,8 +23,6 @@ iptables -A INPUT -i lo -j ACCEPT
 # Accept traffic outgoing (-o) on the loopback (lo) interface ??
 iptables -A OUTPUT -o lo -j ACCEPT
 
-
-
 # Allow established and related connections
 ############################################
 # -m flag allows to match packets based on their connection state
@@ -35,33 +30,47 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+# Allow FCUL's infrastructure traffic
+#######################################
+# Get GCC machine IP address: dig gcc.alunos.di.fc.ul.pt +short
+gcc_ip_address="10.101.151.5"
+
 # Allow ping from the "gcc" machine
 # -s flag allows to specify the source IP address
 # -p icmp --icmp-type echo-request specifies the protocol and the type of ICMP echo request packets
 # echo-request is the ICMP message type for ping
-iptables -A INPUT -s gcc_ip_address -p icmp --icmp-type echo-request -j ACCEPT
-
-# -d flag allows to specify the destination IP address
-# -p icmp --icmp-type echo-reply specifies the protocol and the type of ICMP echo reply packets
-iptables -A OUTPUT -d gcc_ip_address -p icmp --icmp-type echo-reply -j ACCEPT
+iptables -A INPUT -s $gcc_ip_address -p icmp --icmp-type echo-request -j ACCEPT
 
 # Allow SSH from and to the "gcc" machine
 # -p tcp specifies the protocol
 # --dport 22 specifies the destination port
-iptables -A INPUT -s gcc_ip_address -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -s $gcc_ip_address -p tcp --dport 22 -j ACCEPT
 
-# --sport 22 specifies the source port 
-iptables -A OUTPUT -d gcc_ip_address -p tcp --sport 22 -j ACCEPT
+ipset create allowedFculMachines hash:ip
 
+# Data Centers
+ipset add allowedFculMachines 10.121.52.14
+ipset add allowedFculMachines 10.121.52.15
+ipset add allowedFculMachines 10.121.52.16
+# Storage fc-fs03
+ipset add allowedFculMachines 10.121.72.23
+# Falua
+ipset add allowedFculMachines 10.101.85.138
+# Nemo
+ipset add allowedFculMachines 10.101.85.18
+# Gateway submarino
+ipset add allowedFculMachines 10.101.148.1
+# Proxy farol-01
+ipset add allowedFculMachines 10.101.85.137
 
+iptables -A INPUT -m set --match-set allowedFculMachines src -j ACCEPT
+iptables -A OUTPUT -m set --match-set allowedFculMachines src -j ACCEPT
 
 # Allow connections from any origin to myCloud server
-iptables -A OUTPUT -p tcp --sport myCloud_port -j ACCEPT
-
-
+iptables -A INPUT -p tcp --sport myCloud_port -j ACCEPT
 
 # Allow ping to machines in the local subnet with mask 255.255.254.0
-iptables -A OUTPUT -p icmp --icmp-type echo-request -d 255.255.254.0/23 -j ACCEPT
+iptables -A OUTPUT -p icmp --icmp-type echo-request -d 255.255.254.0 -j ACCEPT
 
 # Save the iptables rules
 iptables-save > /etc/iptables/rules.v4
