7a461732fe276c4e8f4780223cefb17c026e2b70
Pedro Almeida <fc56897@alunos.fc.ul.pt>
2023-05-14 18:47:18 +0100

    flush.sh

---
+++ out/production/sinf-projeto/pt/fcul/sinf/si003/server/flush.sh
@@ -0,0 +1,7 @@
+# Script to flush iptables rules
+
+# Set default chain policies to DROP for incoming network traffic
+sudo iptables -A INPUT DROP
+
+# Set default chain policies to DROP for incoming network traffic
+sudo iptables -A OUTPUT DROP
